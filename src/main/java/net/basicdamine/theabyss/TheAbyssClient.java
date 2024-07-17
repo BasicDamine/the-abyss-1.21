bbb2259ebc769e24dc00e5ff8c10deb4ee2b9fcd
BasicDamine <damiongrandon14@gmail.com>
2024-07-16 22:28:10 -0500

    Initial commit

---
+++ src/main/java/net/basicdamine/theabyss/TheAbyssClient.java
@@ -0,0 +1,10 @@
+package net.basicdamine.theabyss;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheAbyssClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
